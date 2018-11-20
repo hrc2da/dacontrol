46579fef103eb696c6f814b05d35234a9fcd2eff
Administrator <administrator@dhcp-rhodes-1897.eduroam.cornell.edu>
2018-11-19 20:09:44 -0500

    fixed block tracker

--- dacontrol/src/components/blockTrackerComponent.js
+++ dacontrol/src/components/blockTrackerComponent.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Card, Table, Button } from 'react-bootstrap';
 
 const BlockTrackerComponent = ({blocks}) => {
+    console.log("BLOCKS",blocks.length);
     const rows = blocks.map((block)=>{
     
-        return  <tr key={block.id+(new Date).getTime().toString()}>
+        return  <tr key={block.id+(Math.random().toString(36).substring(7))}>
                     <td>{block.id}</td>
                     <td>{block.x.toFixed(4)}</td>
                     <td>{block.y.toFixed(4)}</td>
